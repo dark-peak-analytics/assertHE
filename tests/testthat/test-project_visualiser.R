4e216019f3c6a1f2ede45ec927b90c03e9c1c38f
RobertASmith <rsmith@darkpeakanalytics.com>
2024-03-06 17:13:00 +0000

    not sure how commmit this

--- tests/testthat/test-project_visualiser.R
+++ tests/testthat/test-project_visualiser.R
@@ -1,10 +1,10 @@
-test_that("test that nothing blows up with visualiser run ", {
+test_that("Visualiser Sick Sicker ", {
 
-  project_path <- testthat::test_path("example_project")
+  sicksicker_project_path <- testthat::test_path("example_project")
 
   expect_no_error({
 
-  visualise_project(project_path = project_path,
+  visualise_project(project_path = sicksicker_project_path,
                     foo_path = "R",
                     test_path = "tests/testthat",
                     run_coverage = T)
@@ -12,3 +12,22 @@ test_that("test that nothing blows up with visualiser run ", {
   })
 
 })
+
+
+
+test_that("Visualiser cx2cea model ", {
+
+  cdx2cea_project_path <- testthat::test_path("cdx2cea")
+
+  expect_no_error({
+
+    visualise_project(project_path = cdx2cea_project_path,
+                      foo_path = "R",
+                      test_path = "tests/testthat",
+                      run_coverage = F) # note coverage doesn't work on this!
+
+  })
+
+})
+
+
