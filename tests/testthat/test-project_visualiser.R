d1a98b38564744530a4e66cdf588cc76bf9194b3
RobertASmith <rsmith@darkpeakanalytics.com>
2024-03-08 10:48:53 +0000

    turning off code coverage checks

--- tests/testthat/test-project_visualiser.R
+++ tests/testthat/test-project_visualiser.R
@@ -1,13 +1,18 @@
 test_that("test that nothing blows up with visualiser run ", {
+  # ignore if in coverage checks
+  # breaks because we are doing coverage checks within the function itself.
+  testthat::skip_if(covr::in_covr())
 
+  # sicksicker project
   project_path <- testthat::test_path("example_project")
 
   expect_no_error({
-
-  visualise_project(project_path = project_path,
-                    foo_path = "R",
-                    test_path = "tests/testthat",
-                    run_coverage = T)
+    visualise_project(
+      project_path = project_path,
+      foo_path = "R",
+      test_path = "tests/testthat",
+      run_coverage = T
+    )
 
   })
 
