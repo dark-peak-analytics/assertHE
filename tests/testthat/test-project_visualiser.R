305c05cebfd6d6352a7e618bf2279fcec19fd1b1
RobertASmith <rsmith@darkpeakanalytics.com>
2024-03-08 13:03:27 +0000

    ensured code runs even if tests coverage breaks

--- tests/testthat/test-project_visualiser.R
+++ tests/testthat/test-project_visualiser.R
@@ -15,7 +15,7 @@ test_that("Visualiser Sick Sicker ", {
 
 
 
-test_that("Visualiser cx2cea model ", {
+test_that("Visualiser cx2cea model without coverage", {
 
   cdx2cea_project_path <- testthat::test_path("cdx2cea_master")
 
@@ -24,10 +24,32 @@ test_that("Visualiser cx2cea model ", {
     visualise_project(project_path = cdx2cea_project_path,
                       foo_path = "R",
                       test_path = "tests/testthat",
-                      run_coverage = F) # note coverage doesn't work on this!
+                      run_coverage = F)
 
   })
 
 })
+
+
+# NOTE: CANNOT TEST COVERAGE ON VISUALISER BECAUSE OUR TEST TESTS THE COVERAGE
+# WHICH THEN RECALLS TEST. IM SO CONFUSED AND GAVE UP!
+# Need to manually run the code below to check a visual and error message appear.
+
+# test_that("Visualiser cx2cea model with coverage", {
+#   cdx2cea_project_path <- testthat::test_path("cdx2cea_master")
+#
+#   sink("nul")
+#   vis_object <- visualise_project(
+#     project_path = cdx2cea_project_path,
+#     foo_path = "R",
+#     test_path = "tests/testthat",
+#     run_coverage = T
+#   )
+#
+#   sink()
+#
+#   testthat::expect_true("visNetwork" %in% class(vis_object))
+#
+# })
 
 
