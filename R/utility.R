80ec877a99d4bf58f38ea034fb4931a021b08fc6
Smit-tay <damnedyankee@gmail.com>
2024-03-23 16:32:59 +0100

    Clean up annoying issues that were preventing GitHub from marking this change as clean

--- R/utility.R
+++ R/utility.R
@@ -1,17 +1,17 @@
 
 
-#' @title source_files 
+#' @title source_files
 #' @description Source files based upon regular expression searching
 #' # IMPORTANT !!!
 #' sourcing *this* file is a mistake - may result in infinite recursion
 #' @param file_regx = ".*" - a regular expression for files to source
 #' @param path = "." - a path to search
-#' @param recursive = TRUE - recurse into subdirectories 
-#' @param exclude_files = NULL - a regular expression for files to filter (from the results of file_regx)
-#' @param exclude_dirs = NULL - a regular expression for directories to filter (from the results of dir_regx)
+#' @param recursive = TRUE - recurse into subdirectories
+#' @param exclude_files = NULL - regx for files to exclude
+#' @param exclude_dirs = NULL - regx for directories to exclude
 #' @param local = !testthat::is_testing() - see base::source() for details
 #' @param verbose = FALSE - whether to emit the sourced files.
-#' 
+#'
 #' @return list of files sourced
 #'
 #' @export
@@ -21,20 +21,20 @@
 #' \dontrun{
 #' source_files(file_regx = ".*",  ## any file name
 #'  path = ".*",   # the current directory and all subdirectories
-#'  recursive = FALSE  # don't recurse
-#'  exclude_files = ".*utility.*",   # don't source any file with the string "utility" anywhere in its basename
-#'  exclude_dirs = "\/tmp\/"  # don't source any files which reside in any directory named "tmp", or its subdirectories
-#' )
+#'  recursive = FALSE,  # don't recurse
+#'  exclude_files = ".*utility.*", # exclude "utility" anywhere in basename
+#'  exclude_dirs = "\/tmp\/"  # exclude any directory named "tmp", or subdirs
+#'  )
 #' }
 #'
-source_files <- function( file_regx = ".*",  
-                          path = ".", 
-                          recursive = TRUE, 
-                          exclude_files = NULL, 
-                          exclude_dirs = NULL, 
-                          local=!testthat::is_testing(), 
+source_files <- function( file_regx = ".*",
+                          path = ".",
+                          recursive = TRUE,
+                          exclude_files = NULL,
+                          exclude_dirs = NULL,
+                          local=!testthat::is_testing(),
                           verbose=FALSE) {
-  
+
   # Get the list of files matching file_regx in directories matching dir_regx
   files <- list.files(path = path, pattern = file_regx, recursive=recursive, full.names = TRUE)
 
@@ -45,7 +45,7 @@ source_files <- function( file_regx = ".*",
   if (!is.null(exclude_files)) {
     files <- files[!grepl(exclude_files, basename(files))]
   }
-  
+
   # Filter out any directories which match the Directory exclude regex
   if (!is.null(exclude_dirs)) {
     files <- files[!grepl(exclude_dirs, dirname(files))]
