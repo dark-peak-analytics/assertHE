8fd263d618728d6e86a8915b313ff0985565c457
Smit-tay <damnedyankee@gmail.com>
2024-07-09 13:49:20 +0200

    fix warnings about missing namespace

--- R/addin_handler.R
+++ R/addin_handler.R
@@ -2,41 +2,41 @@
 
 # Addin handler
 visualise_project_addin <- function() {
-  library(shiny)
-  ui <- fluidPage(
-    titlePanel("Visualise Project"),
-    sidebarLayout(
-      sidebarPanel(
-        textInput("project_path", "Project Path", value = ""),
-        textInput("foo_path", "Foo Path", value = "R"),
-        textInput("test_path", "Test Path", value = NULL),
-        textInput("exclude_files", "Exclude Files (comma-separated)", value = NULL),
-        textInput("exclude_dirs", "Exclude Dirs (comma-separated)", value = NULL),
-        checkboxInput("run_coverage", "Run Coverage", value = FALSE),
-        textInput("color_no_test_bg", "No Test Background Color", value = "#fad1d0"),
-        textInput("color_no_test_border", "No Test Border Color", value = "#9c0000"),
-        textInput("color_no_test_highlight", "No Test Highlight Color", value = "#9c0000"),
-        textInput("color_with_test_bg", "With Test Background Color", value = "#e6ffe6"),
-        textInput("color_with_test_border", "With Test Border Color", value = "#65a765"),
-        textInput("color_with_test_highlight", "With Test Highlight Color", value = "#65a765"),
-        textInput("color_mod_coverage_bg", "Moderate Coverage Background Color", value = "#FFD580"),
-        textInput("color_mod_coverage_border", "Moderate Coverage Border Color", value = "#E49B0F"),
-        textInput("color_mod_coverage_highlight", "Moderate Coverage Highlight Color", value = "#E49B0F"),
-        sliderInput("moderate_coverage_range", "Moderate Coverage Range", min = 0, max = 1, value = c(0.2, 0.8)),
-        checkboxInput("print_isolated_foo", "Print Isolated Foo", value = TRUE),
-        checkboxInput("show_in_shiny", "Show in Shiny", value = FALSE),
-        textInput("network_title", "Network Title", value = "Function Network"),
-        checkboxInput("scale_node_size_by_degree", "Scale Node Size by Degree", value = TRUE),
-        actionButton("visualize", "Visualize")
+
+  ui <- shiny::fluidPage(
+    shiny::titlePanel("Visualise Project"),
+    shiny::sidebarLayout(
+      shiny::sidebarPanel(
+        shiny::textInput("project_path", "Project Path", value = ""),
+        shiny::textInput("foo_path", "Foo Path", value = "R"),
+        shiny::textInput("test_path", "Test Path", value = NULL),
+        shiny::textInput("exclude_files", "Exclude Files (comma-separated)", value = NULL),
+        shiny::textInput("exclude_dirs", "Exclude Dirs (comma-separated)", value = NULL),
+        shiny::checkboxInput("run_coverage", "Run Coverage", value = FALSE),
+        shiny::textInput("color_no_test_bg", "No Test Background Color", value = "#fad1d0"),
+        shiny::textInput("color_no_test_border", "No Test Border Color", value = "#9c0000"),
+        shiny::textInput("color_no_test_highlight", "No Test Highlight Color", value = "#9c0000"),
+        shiny::textInput("color_with_test_bg", "With Test Background Color", value = "#e6ffe6"),
+        shiny::textInput("color_with_test_border", "With Test Border Color", value = "#65a765"),
+        shiny::textInput("color_with_test_highlight", "With Test Highlight Color", value = "#65a765"),
+        shiny::textInput("color_mod_coverage_bg", "Moderate Coverage Background Color", value = "#FFD580"),
+        shiny::textInput("color_mod_coverage_border", "Moderate Coverage Border Color", value = "#E49B0F"),
+        shiny::textInput("color_mod_coverage_highlight", "Moderate Coverage Highlight Color", value = "#E49B0F"),
+        shiny::sliderInput("moderate_coverage_range", "Moderate Coverage Range", min = 0, max = 1, value = c(0.2, 0.8)),
+        shiny::checkboxInput("print_isolated_foo", "Print Isolated Foo", value = TRUE),
+        shiny::checkboxInput("show_in_shiny", "Show in Shiny", value = FALSE),
+        shiny::textInput("network_title", "Network Title", value = "Function Network"),
+        shiny::checkboxInput("scale_node_size_by_degree", "Scale Node Size by Degree", value = TRUE),
+        shiny::actionButton("visualize", "Visualize")
       ),
-      mainPanel(
-        plotOutput("network_plot")
+      shiny::mainPanel(
+        shiny::plotOutput("network_plot")
       )
     )
   )
 
   server <- function(input, output, session) {
-    observeEvent(input$visualize, {
+    shiny::observeEvent(input$visualize, {
       exclude_files <- ifelse(is.null(input$exclude_files), NULL, unlist(strsplit(input$exclude_files, ",")))
       exclude_dirs <- ifelse(is.null(input$exclude_dirs), NULL, unlist(strsplit(input$exclude_dirs, ",")))
 
@@ -63,5 +63,5 @@ visualise_project_addin <- function() {
     })
   }
 
-  shinyApp(ui = ui, server = server)
+  shiny::shinyApp(ui = ui, server = server)
 }
