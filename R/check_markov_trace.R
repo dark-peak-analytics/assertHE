d13226cace4da8a4ede31ddd8f1a58c5da5e2e4e
RobertASmith <rsmith@darkpeakanalytics.com>
2024-07-25 10:56:39 +0100

    edit markov trace checker

--- R/check_markov_trace.R
+++ R/check_markov_trace.R
@@ -52,7 +52,7 @@ check_markov_trace <- function(m_TR,
   no_warnings <- T
 
   # Check that the matrix contains numeric values
-  if (!is.numeric(m_TR))  stop("Markov trace is not numeric")
+  if (!all(apply(m_TR, MARGIN = 2, is.numeric)))  stop("Markov trace is not numeric")
 
   # Check that matrix values are between 0 and 1
   if (!all(m_TR >= 0 & m_TR <= 1)) {
